6067e7f707c0f096dd033dc1d44cd96f488a7318
owuama1 <kelechiowuama24@gmail.com>
2024-06-17 11:44:16 +0000

    make it compatible with welzburg

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Using Puppet, install flask from pip3
-package { 'flask':
+# This Puppet manifest installs Flask version 2.1.0 and Werkzeug 2.0.3 using pip3.
+package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['Werkzeug'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.0.3',
   provider => 'pip3',
 }
