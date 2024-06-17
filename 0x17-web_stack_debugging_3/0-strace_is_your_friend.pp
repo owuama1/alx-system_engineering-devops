1db1455196a1faf55cf7723c484c256d73bda232
owuama1 <kelechiowuama24@gmail.com>
2024-06-17 12:49:09 +0000

    edit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,18 +1,10 @@
-# This Puppet manifest ensures the /var/www/html directory exists and is owned by the www-data user and group.
-file { '/var/www/html':
-  ensure  => 'directory',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0755',
-}
+# A puppet manuscript to replace a line in a file on a server
 
-# Ensure Apache is installed and running
-package { 'apache2':
-  ensure => 'installed',
-}
+$file_to_edit = '/var/www/html/wp-settings.php'
 
-service { 'apache2':
-  ensure => 'running',
-  enable => true,
-  require => Package['apache2'],
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
